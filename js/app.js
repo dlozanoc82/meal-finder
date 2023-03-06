07807123cf70cdbd72e7b1ba3821d1feb15f32cc
Daniel <dlozanoc862@gmail.com>
2023-03-05 20:39:37 -0500

    :sparkles: Categories are displayed in the Select

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
 function startApp() {
-    
+
+    //Vars
+    const buttonMenu = document.querySelector('#buttonMenu'); 
+    const mobileMenu = document.querySelector('.navbar__mobile');
+    const closeMenuButton = document.querySelector('#closeMenuButton');
+    const selectCategorias = document.querySelector('#categorias');
+
+    //Listers
+    buttonMenu.addEventListener('click', toggleMobileMenu);
+    closeMenuButton.addEventListener('click', toggleMobileMenu);
+
     getCategory();
+
+    // Functions 
+    function toggleMobileMenu(){
+        mobileMenu.classList.toggle('inactive');
+    }
 
     function getCategory() {
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
         fetch(url)
+            .then(response => response.json())
+            .then(result => setCategory(result.categories))
+    }
+
+    function setCategory(categories = []) {
+        categories.forEach(category => {
+            const { strCategory } = category;
+            const option = document.createElement('option');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            selectCategorias.appendChild(option);
+        })
     }
 
 }
 
 document.addEventListener('DOMContentLoaded', startApp);
-
-const buttonMenu = document.querySelector('#buttonMenu'); 
-const mobileMenu = document.querySelector('.navbar__mobile');
-const closeMenuButton = document.querySelector('#closeMenuButton');
-
-buttonMenu.addEventListener('click', toggleMobileMenu);
-closeMenuButton.addEventListener('click', toggleMobileMenu);
-
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
-}
